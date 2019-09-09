d3ec139aea6d5aeab5c097ef322f0d5431e45e6f
LeeJaeBae <nea4182@gmail.com>
2019-09-09 12:04:15 +0900

    스토어 생성

---
+++ src/store/modules/post.js
@@ -0,0 +1,14 @@
+import { createAction, handleActions } from "redux-action";
+
+import { Map } from "immutable";
+import { pender } from "redux-pender";
+
+// action types
+
+// action creators
+
+// initial state
+const initaionState = Map({});
+
+// render
+export default handleActions({}, initaionState);
